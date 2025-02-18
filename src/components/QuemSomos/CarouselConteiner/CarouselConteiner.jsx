67d2e5860f3bb4442ccfa4a81c99afabbc312828
Pablo Guimarães <145077458+Pablo01Dev@users.noreply.github.com>
2025-02-18 13:20:43 -0300

    ajustando carousel

--- src/components/QuemSomos/CarouselConteiner/CarouselConteiner.jsx
+++ src/components/QuemSomos/CarouselConteiner/CarouselConteiner.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useInView } from 'react-intersection-observer';
 import styles from './CarouselConteiner.module.css';
 import { carouselHorizontal, carouselVertical } from '../../../data';
 
 function CarouselConteiner() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const { ref, inView } = useInView({ triggerOnce: true }); // Monitorar quando o carrossel estiver visível
+
+    useEffect(() => {
+        let intervalId;
+        if (inView) { // Inicia o carrossel apenas quando visível
+            intervalId = setInterval(() => {
+                setCurrentIndex((prevIndex) => (prevIndex + 1) % carouselVertical.length); 
+            }, 2000);
+        } else {
+            clearInterval(intervalId); // Para o carrossel se não estiver visível
+        }
+
+        return () => clearInterval(intervalId); // Limpa o intervalo quando o componente for desmontado ou invisível
+    }, [inView]); // Re-efetua o efeito sempre que a visibilidade mudar
+
     return (
-        <div className={styles.carouselContainer}>
+        <div className={styles.carouselContainer} ref={ref}>
             {/* Coluna da esquerda - Imagens horizontais */}
             <div className={styles.carouselLeftRight}>
                 {carouselHorizontal.slice(0, 2).map((item) => (
@@ -16,7 +33,11 @@ function CarouselConteiner() {
 
             {/* Carrossel central - Imagem vertical */}
             <div className={styles.carouselItemCenter}>
-                <img src={carouselVertical[0].image} alt={carouselVertical[0].title} className={styles.carouselImage} />
+                <img 
+                    src={carouselVertical[currentIndex].image} 
+                    alt={carouselVertical[currentIndex].title} 
+                    className={styles.carouselImage} 
+                />
             </div>
 
             {/* Coluna da direita - Imagens horizontais */}
