1cc7ae5803b6f2ddd82b5aa0e068f2855477611f
Pablo Guimarães <145077458+Pablo01Dev@users.noreply.github.com>
2025-02-24 20:17:06 -0300

    ajustes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import styles from './Contact.module.css'; // Importe seu CSS aqui, se necessário
+import styles from './Contact.module.css';
 
 function Contact() {
     return (
-        <div className={styles.contactContainer}> 
+        <div className={styles.contactContainer}>
+            <div className={styles.line}></div>
             <h1>Quer levar drinks incríveis para o seu evento?</h1>
-            <h2>CLIQUE AQUI e solicite o seu orçamento!</h2>
-
-            <img src="caminho/para/sua/imagem.jpg" alt="Descrição da imagem" /> 
-
+            <h2>
+                <a href="#">CLIQUE AQUI</a> e solicite o seu orçamento!
+            </h2>
             <div className={styles.contactInfo}>
-                <h2>Trabalhe Conosco</h2>
                 <h2>atendimentorj@newopenbar.com.br</h2>
-            </div>
-
-            <div className={styles.local}>
-                <h3>Rua Siqueira Campos 43, sala 932 – Copacabana, Rio de Janeiro – CEP: 22011-060</h3>
+                <div className={styles.local}>
+                    <h3>Rua Siqueira Campos 43, sala 932 – Copacabana, Rio de Janeiro – CEP: 22011-060</h3>
+                </div>
+                <button>
+                    <a href="https://api.whatsapp.com/send?phone=5521991797829&text=Ol%C3%A1%2C%20quero%20trabalhar%20com%20a%20*New%20Open*!">
+                        Trabalhe Conosco
+                    </a>
+                </button>
             </div>
         </div>
     );
