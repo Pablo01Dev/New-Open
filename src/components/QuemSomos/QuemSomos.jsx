b514b33ed785d11084e4d432fbaf7540e1b1cc5e
Pablo Guimarães <145077458+Pablo01Dev@users.noreply.github.com>
2025-02-26 19:27:11 -0300

    ajustes finais

--- src/components/QuemSomos/QuemSomos.jsx
+++ src/components/QuemSomos/QuemSomos.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import CarouselConteiner from './CarouselConteiner/CarouselConteiner.jsx';
 import styles from './QuemSomos.module.css';
 
@@ -6,30 +6,6 @@ function QuemSomos() {
     const textRef = useRef(null);
     const h2Ref = useRef(null);
     const pRef = useRef(null);
-
-    useEffect(() => {
-        const elements = [textRef.current, h2Ref.current, pRef.current];
-
-        function checkVisibility() {
-            elements.forEach(element => {
-                if (element) {
-                    const rect = element.getBoundingClientRect();
-                    const isVisible = rect.top <= window.innerHeight && rect.bottom >= 0;
-
-                    if (isVisible) {
-                        element.classList.add(styles.visible);
-                    }
-                }
-            });
-        }
-
-        checkVisibility();
-        window.addEventListener('scroll', checkVisibility);
-
-        return () => {
-            window.removeEventListener('scroll', checkVisibility);
-        };
-    }, []);
 
     return (
         <div className={styles.quemSomosContainer}>
@@ -41,7 +17,7 @@ function QuemSomos() {
                 <p ref={pRef}>
                     Beber sempre fez parte dos momentos de celebração, e os coquetéis
                     são uma fusão de estilos e tendências que atravessaram gerações.
-                    <br/><br/>
+                    <br /><br />
                     Nós trazemos o melhor da coquetelaria clássica e adicionamos releituras
                     modernas e exclusivas, criando drinks que fazem do seu evento uma
                     experiência única e inesquecível!
